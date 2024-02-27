67055a17605fc89306249e1885346324a08287ea
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-02-27 10:18:14 -0300

    feat: add CountryInfo component

---
+++ src/components/CountryInfo.tsx
@@ -0,0 +1,31 @@
+import { Countries } from "../utils/types"
+
+type CountryInfoProps = {
+    data: Countries;
+}
+
+const CountryInfo = ({ data }: CountryInfoProps) => {
+
+    const countryCurrency = data && data.currencies[Object.keys(data.currencies)[0]].name;
+    const countryLanguages = data && Object.keys(data.languages)
+    
+    return (
+        <div>
+            <h1>{data.name.official}</h1>
+            <div>
+                <p>Native Name: <span>{data.name.official}</span></p>
+                <p>Population: <span>{data.population}</span></p>
+                <p>Region: <span>{data.region}</span></p>
+                <p>Sub Region: <span>{data.subregion}</span></p>
+                <p>Capital: <span>{data.capital}</span></p>
+            </div>
+            <div>
+                <p>Top Level Domain: <span>{data.name.common}</span></p>
+                <p>Currencies: <span>{countryCurrency}</span></p>
+                <p>Languages: {countryLanguages && countryLanguages.map(lang => <span key={lang}> {lang} </span>)}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CountryInfo
