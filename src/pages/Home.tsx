0f228f1986bc98a69e741577c2faef66330a3b03
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-02-27 09:53:43 -0300

    feat: add hook for fetching data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,18 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 import { Countries } from "../utils/types";
 import Search from "../components/Search";
 import Filter from "../components/Filter";
 import Card from "../components/Card";
+import { useFetch } from "../utils/useFetch";
 
 const Home = () => {
-    const [data, setData] = useState<Countries[] | null>(null);
-    useEffect( () => {      
-
-      const fetchApi = async () => {
-        try{
-          const fetchData = await fetch('https://restcountries.com/v3.1/all');
-          const data = await fetchData.json();
-          setData(data);
-          localStorage.setItem('data', JSON.stringify(data));
-        }catch(err){
-          alert(err)
-        }
-      }
-
-      fetchApi();
-
-    }, [])
-
+  const {data, loading} = useFetch<Countries[]>('all')
   return (
     <main className='container p-4 flex flex-col'>
         <Search />
         <Filter />
-        {data && data.map(country => <Card country={country}/> )}
+        <div>{loading && 'loading...'}</div>
+        {data && data.map(country => <Card key={country.cca2} country={country} /> )}
     </main>
   )
 }
---
+++ src/utils/useFetch.ts
@@ -0,0 +1,22 @@
+
+import { useEffect, useState } from "react";
+
+
+export function useFetch<T = unknown>(urlString: string) {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null)
+
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/${urlString}`)
+            .then(res => res.json())
+            .then(res => {
+                setData(res)
+                setLoading(false);
+            })
+            .catch(err => setError(err))
+            .finally(() => setLoading(false))
+    });
+
+    return { data, loading, error }
+}
